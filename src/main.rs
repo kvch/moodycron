db854649ca5074520cd8dcb1670e544789d3be1b
Noémi Ványi <sitbackandwait@gmail.com>
2025-04-18 16:44:42 +0200

    something is working

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,29 @@
 // TODO run as daemon
-// TODO open crontab
 // TODO look for update of cron tab file
 // TODO track programs to run
 // TODO run programs
 // TODO log moods, stamina
+use chrono::Utc;
 use cron::Schedule;
-//use crono::Utc;
 use std::fs::File;
 use std::io::prelude::*;
-use std::splitn;
+use std::process::Command;
 use std::str::FromStr;
+use std::thread;
 
-// Cron with stamina
-// Cron with moods
+struct CronStats {
+    stamina: u8,
+    reflexes: u8,
+    dexterity: u8,
+}
 
-//fn read_crontab() -> Option<String> {
-//    let mut file = match File::open("cron") {
-//        Ok(file) => file,
-//        Err(_) => return None,
-//    };
-//    let mut contents = String::new();
-//    match file.read_to_string(&mut contents) {
-//        Ok(_) => return Some(contents),
-//        Err(_) => return None,
-//    };
-//}
+fn parse_cron_line(cron_line: String) -> (String, String) {
+    let mut cron_expression: Vec<&str> = (&cron_line).splitn(7, " ").collect();
+    let cmd_expression = cron_expression.pop().unwrap();
+    let schedule_expression = cron_expression.into_iter().collect::<String>();
+    return (schedule_expression, cmd_expression.to_string());
+}
+
 fn read_crontab() -> Option<String> {
     let mut file = File::open("cron").ok()?;
     let mut contents = String::new();
@@ -33,12 +32,22 @@ fn read_crontab() -> Option<String> {
 }
 
 fn main() {
-    println!("Hello, world!");
     let contents = match read_crontab() {
         Some(contents) => contents,
-        None => String::new(),
+        None => return,
     };
-    println!("AFDAFS {}", contents);
-    let cron_expression = splitn(&contents.to_owned(), " ", 6);
-    println!("{}", cron_expression);
+    let (schedule_expression, cmd_expression) = parse_cron_line(contents);
+    let schedule = Schedule::from_str(&schedule_expression).unwrap();
+    for datetime in schedule.upcoming(Utc).take(1) {
+        let now = Utc::now();
+        let until = datetime - now;
+        thread::sleep(until.to_std().unwrap());
+        let cron_cmd_output = Command::new("sh")
+            .arg("-c")
+            .arg(&cmd_expression)
+            .output()
+            .unwrap()
+            .stdout;
+        println!("{}", String::from_utf8(cron_cmd_output).unwrap());
+    }
 }
